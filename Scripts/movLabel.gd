6e613d35e7f64a480180624c08065728810f013b
Martim Emauz <martim.emauz@gmail.com>
2024-11-19 18:56:16 +0000

    Text animations

--- Scripts/movLabel.gd
+++ Scripts/movLabel.gd
@@ -1,8 +1,16 @@
 extends Label
 
+@onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
+
+var pressed = false
+
+# Fazer a label desaparecer quando o jogador pressiona "A" ou "D"
 func _process(delta: float) -> void:
-	#Fazer a label desaparecer quando o jogador pressiona "A" ou "D"
-	if Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_right"):
-		pass
-		
-		
+	if (Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_right")) and not pressed:
+		print("text fading out...")
+		pressed = true
+		_fade_out(self)
+
+# Fazer a animação de esconder o texto
+func _fade_out(body):
+	animation_player.play("hide")
