f921c65316ea6653189f669be65d25ba0e6850fb
Martim Emauz <martim.emauz@gmail.com>
2024-12-18 00:42:13 +0000

    Changes to menu, tutorial level
    Made menu fit to screen always;
    Added a mini puzzle to the tutorial (door to open with box);
    + Bug fixes and some scenes were changed

--- Scripts/interactive_door.gd
+++ Scripts/interactive_door.gd
@@ -1,6 +1,7 @@
-extends AnimatedSprite2D
+extends StaticBody2D
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 @export var isOpen = false
 
@@ -9,15 +10,30 @@ signal toggle_door # Pode ser usado por outros nodes, funciona como um trigger
 func _ready() -> void:
 	# Verifica que a porta é inicializada com o valor certo
 	update_door_state()
+	
+func open():
+	if !isOpen:
+		animated_sprite.play("Open")
+		isOpen = true
+		collision_shape_2d.set_deferred("disabled", isOpen)
+
+
+func close():
+	if isOpen:
+		animated_sprite.play("Close")
+		isOpen = false
+		collision_shape_2d.set_deferred("disabled", isOpen)
+
 
 func open_or_close():
 	if isOpen:
-		play("Open")
+		animated_sprite.play("Open")
 	else:
-		play("Close")
+		animated_sprite.play("Close")
 
 	# define a colisão conforme o valor da variável
-	collision_shape_2d.disabled = isOpen
+	collision_shape_2d.set_deferred("disabled", isOpen)
+
 
 func _on_toggle_door() -> void:
 	# Muda o estado da variável e atualiza o valor da porta
@@ -27,3 +43,10 @@ func _on_toggle_door() -> void:
 func update_door_state():
 	# Chama a função que abre ou fecha a porta
 	open_or_close()
+
+
+func _on_trigger_platform_activated() -> void:
+	open()
+	
+func _on_trigger_platform_deactivated() -> void:
+	close()
