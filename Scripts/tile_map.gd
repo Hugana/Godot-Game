7ad3e3428fabc3c58a8e593f253a16051facb9fd
Martim Emauz <martim.emauz@gmail.com>
2025-01-11 04:18:16 +0000

    X-ray logic done

--- Scripts/tile_map.gd
+++ Scripts/tile_map.gd
@@ -1,32 +1,32 @@
 extends TileMap
 
-# Signal whether X-Ray mode is active
 var x_ray_active = false
 var tangible_layer = 0  # Replace with your tangible layer index
-#@onready var player = get_tree().get_first_node_in_group("player")
-# Add a reference to the player's collision (assuming player is a CharacterBody2D)
 @export var player: CharacterBody2D  # Make sure this is the correct node type
 
-func _process(delta):
-	# Check if the toggle input is pressed
-	if Input.is_action_just_pressed("x_ray_camera_toggle"):
-		toggle_x_ray_mode()
-		print("X-ray toggled")
+func _ready():
+	if player:
+		player.connect("xray_toggled", Callable(self, "_on_xray_toggled"))
 
-func toggle_x_ray_mode():
-	x_ray_active = !x_ray_active
+func _process(delta):
+	# If you want to handle something else in the TileMap, you can do it here.
+	pass
+
+# Handle when the X-ray is toggled
+func _on_xray_toggled(xray_status: bool) -> void:
+	x_ray_active = xray_status
 	print("X-Ray Active:", x_ray_active)
 
 	# Ensure the player node is not Nil before proceeding
 	if player:
 		# Modify the player's collision mask based on X-ray mode
 		if x_ray_active:
-			# Disable collision with the Collision layer (layer 0)
-			player.collision_mask &= ~(1 << 1)
+			# Disable collision with the Tangible layer (layer 1 in this case)
+			player.collision_mask &= ~(1 << tangible_layer)
 			print("Player collision with Tangible layer disabled.")
 		else:
-			# Restore collision with the Collision layer (layer 0)
-			player.collision_mask |= (1 << 1) 
+			# Restore collision with the Tangible layer (layer 1)
+			player.collision_mask |= (1 << tangible_layer)
 			print("Player collision with Tangible layer enabled.")
 	else:
 		print("Warning: Player node is not assigned!")
